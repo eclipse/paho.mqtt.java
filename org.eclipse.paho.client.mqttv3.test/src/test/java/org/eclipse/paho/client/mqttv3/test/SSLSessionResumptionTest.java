e577b9004dfa708e43d466b8c2d4dcac528e5a6a
James Sutton <james.sutton@uk.ibm.com>
2016-09-06 13:52:31 +0100

    Fixing SSLSessionResumptionTest to use keystore
    Signed-off-by: James Sutton <james.sutton@uk.ibm.com>

--- org.eclipse.paho.client.mqttv3.test/src/test/java/org/eclipse/paho/client/mqttv3/test/SSLSessionResumptionTest.java
+++ org.eclipse.paho.client.mqttv3.test/src/test/java/org/eclipse/paho/client/mqttv3/test/SSLSessionResumptionTest.java
@@ -13,17 +13,15 @@
 
 package org.eclipse.paho.client.mqttv3.test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.SecureRandom;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSessionContext;
 import javax.net.ssl.SSLSocket;
@@ -38,156 +36,145 @@ import org.eclipse.paho.client.mqttv3.test.utilities.Utility;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 /**
  * This test aims to run some basic SSL functionality tests of the MQTT client
  */
 
 public class SSLSessionResumptionTest {
 
-  static final Class<?> cclass = SSLSessionResumptionTest.class;
-  private static final String className = cclass.getName();
-  private static final Logger log = Logger.getLogger(className);
-  
-  
-  private static String serverURI;
-  private static String serverHost;
-  private static int serverPort;
-  private static final String certificateName = "iot.eclipse.org.crt";
+	static final Class<?> cclass = SSLSessionResumptionTest.class;
+	private static final String className = cclass.getName();
+	private static final Logger log = Logger.getLogger(className);
 
-  
-  /**
-   * @throws Exception 
-   */
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
+	private static String serverURI;
+	private static String serverHost;
+	private static int serverPort;
 
-    try {
-      String methodName = Utility.getMethodName();
-      LoggingUtilities.banner(log, cclass, methodName);
-      serverURI = "ssl://" + TestProperties.getServerURI().getHost();
-      serverHost = TestProperties.getServerURI().getHost();
-      serverPort = TestProperties.getServerSSLPort();
+	/**
+	 * @throws Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 
-    }
-    catch (Exception exception) {
-      log.log(Level.SEVERE, "caught exception:", exception);
-      throw exception;
-    }
-  }
+		try {
+			String methodName = Utility.getMethodName();
+			LoggingUtilities.banner(log, cclass, methodName);
+			serverURI = "ssl://" + TestProperties.getServerURI().getHost();
+			serverHost = TestProperties.getServerURI().getHost();
+			serverPort = TestProperties.getServerSSLPort();
 
-  /**
-   * This test involves inspecting the SSL debug log
-   * and the Wireshark capture.
-   *
-   * Paho defeats the default SSL session caching which
-   * allows to have abbreviated SSL handshakes on
-   * following connections.
-   * 
-   * @throws Exception
-   */
-  @Test
-  public void testSSLSessionInvalidated() throws Exception {
-	  //System.setProperty("javax.net.debug", "all");
+		} catch (Exception exception) {
+			log.log(Level.SEVERE, "caught exception:", exception);
+			throw exception;
+		}
+	}
 
-	  SSLSocketFactory factory = getSocketFactory(certificateName);
-	  
-	  MqttConnectOptions options = new MqttConnectOptions();
-	  options.setServerURIs(new String[] {serverURI});
-	  options.setKeepAliveInterval(60);
-	  options.setSocketFactory(factory);
-	  
-	  MqttClient mqttClient = new MqttClient(serverURI, MqttClient.generateClientId());
-	  
-	  
-	  log.info("Connecting to: " + serverURI);
-	  mqttClient.connect(options);
-	  
-	  Thread.sleep(2000);
-	  
-	  log.info("Disconnetting...");
-	  mqttClient.disconnect();
-	  	  
-	  Thread.sleep(2000);
-	  
-	  log.info("Connecting again... Paho will not be able to perform an abbreviated SSL handshake");
-	  mqttClient.connect(options);
-	  
-	  Thread.sleep(2000);
-	  
-	  log.info("Disconnetting...");
-	  mqttClient.disconnect();
-	  
-  }
+	/**
+	 * This test involves inspecting the SSL debug log and the Wireshark
+	 * capture.
+	 *
+	 * Paho defeats the default SSL session caching which allows to have
+	 * abbreviated SSL handshakes on following connections.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testSSLSessionInvalidated() throws Exception {
+		// System.setProperty("javax.net.debug", "all");
 
-  /**
-   * This test involves inspecting the SSL debug log
-   * and the Wireshark capture.
-   *
-   * By default, Java caches SSL sessions which
-   * allows to have abbreviated SSL handshakes on
-   * following connections.
-   * 
-   * @throws Exception
-   */
-  @Test
-  public void testSSLSessionCached() throws Exception {
-	 // System.setProperty("javax.net.debug", "all");
-	  
-	  SSLSocketFactory factory = getSocketFactory(certificateName);
+		SSLSocketFactory factory = getSocketFactory();
 
-	  
-	  log.info("Do handshake...");
-	  doHandshake(factory, serverHost, serverPort);
+		MqttConnectOptions options = new MqttConnectOptions();
+		options.setServerURIs(new String[] { serverURI });
+		options.setKeepAliveInterval(60);
+		options.setSocketFactory(factory);
 
-	  log.info("Done! Redo handshake... An abbreviated SSL handshake will be performed");
-	  doHandshake(factory, serverHost, serverPort);
-	  
-	  log.info("Done!");
-  }
-  
-  private static void doHandshake(SSLSocketFactory factory, String host, int port) {
-	  SSLSocket socket = null;
-	  try {
-		  socket = (SSLSocket) factory.createSocket(host, port);
+		MqttClient mqttClient = new MqttClient(serverURI, MqttClient.generateClientId());
 
-		  socket.startHandshake();
+		log.info("Connecting to: " + serverURI);
+		mqttClient.connect(options);
 
-		  SSLSessionContext ctx = socket.getSession().getSessionContext();
-		  if (ctx != null) {
-			  Enumeration<byte[]> ids = ctx.getIds();
-			  while (ids.hasMoreElements()) {
-				  byte[] id = ids.nextElement();
-				  log.info("Session ID: " + Arrays.toString(id));
-				  log.info("Cypher suite: " + ctx.getSession(id).getCipherSuite());
-			  }
-		  } else {
-			  log.info("null SSLSessionContext");
-		  }
-	  } catch (Exception e) {
-		  e.printStackTrace();
-	  } finally {
-		  if (socket != null) {
-			  try {
-				socket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+		Thread.sleep(2000);
+
+		log.info("Disconnetting...");
+		mqttClient.disconnect();
+
+		Thread.sleep(2000);
+
+		log.info("Connecting again... Paho will not be able to perform an abbreviated SSL handshake");
+		mqttClient.connect(options);
+
+		Thread.sleep(2000);
+
+		log.info("Disconnetting...");
+		mqttClient.disconnect();
+
+	}
+
+	/**
+	 * This test involves inspecting the SSL debug log and the Wireshark
+	 * capture.
+	 *
+	 * By default, Java caches SSL sessions which allows to have abbreviated SSL
+	 * handshakes on following connections.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testSSLSessionCached() throws Exception {
+		// System.setProperty("javax.net.debug", "all");
+
+		SSLSocketFactory factory = getSocketFactory();
+
+		log.info("Do handshake...");
+		doHandshake(factory, serverHost, serverPort);
+
+		log.info("Done! Redo handshake... An abbreviated SSL handshake will be performed");
+		doHandshake(factory, serverHost, serverPort);
+
+		log.info("Done!");
+	}
+
+	private static void doHandshake(SSLSocketFactory factory, String host, int port) {
+		SSLSocket socket = null;
+		try {
+			socket = (SSLSocket) factory.createSocket(host, port);
+
+			socket.startHandshake();
+
+			SSLSessionContext ctx = socket.getSession().getSessionContext();
+			if (ctx != null) {
+				Enumeration<byte[]> ids = ctx.getIds();
+				while (ids.hasMoreElements()) {
+					byte[] id = ids.nextElement();
+					log.info("Session ID: " + Arrays.toString(id));
+					log.info("Cypher suite: " + ctx.getSession(id).getCipherSuite());
+				}
+			} else {
+				log.info("null SSLSessionContext");
 			}
-		  }
-	  }
-  }
-  
-  private static SSLSocketFactory getSocketFactory(String certificateName) throws Exception {
-  		InputStream certStream = SSLSessionResumptionTest.class.getClassLoader().getResourceAsStream(certificateName);
-  		CertificateFactory certFactory = CertificateFactory.getInstance("X509");
-      Certificate certificate =  certFactory.generateCertificate(certStream);
-      SSLContext sslContext = SSLContext.getInstance("TLS");
-      TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-  		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-  		keyStore.load(null);
-  		keyStore.setCertificateEntry("alias", certificate);
-  		trustManagerFactory.init(keyStore);
-  		sslContext.init(null, trustManagerFactory.getTrustManagers(), new SecureRandom());
-  		return sslContext.getSocketFactory();
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private static SSLSocketFactory getSocketFactory() throws Exception {
+		InputStream keyStoreStream = new FileInputStream(TestProperties.getClientKeyStore());
+		SSLContext sslContext = SSLContext.getInstance("TLS");
+		TrustManagerFactory trustManagerFactory = TrustManagerFactory
+				.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		keyStore.load(keyStoreStream, TestProperties.getClientKeyStorePassword().toCharArray());
+		trustManagerFactory.init(keyStore);
+		sslContext.init(null, trustManagerFactory.getTrustManagers(), new SecureRandom());
+		return sslContext.getSocketFactory();
+	}
 }
