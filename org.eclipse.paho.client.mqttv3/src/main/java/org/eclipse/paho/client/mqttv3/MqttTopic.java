e5e7e93206525160a94e83d44f64897c8f40743e
James Sutton <jpwsutton@gmail.com>
2016-09-30 13:49:27 +0100

    Fixing paho java client to minimum JRE 1.4.2+ source (Issue #218) (#273)
    * Fixing mqttv3 source to be compatible with Java 1.4.2+
    
    Signed-off-by: James Sutton <james.sutton@uk.ibm.com>

--- org.eclipse.paho.client.mqttv3/src/main/java/org/eclipse/paho/client/mqttv3/MqttTopic.java
+++ org.eclipse.paho.client.mqttv3/src/main/java/org/eclipse/paho/client/mqttv3/MqttTopic.java
@@ -146,7 +146,8 @@ public class MqttTopic {
 		try {
 			topicLen = topicString.getBytes("UTF-8").length;
 		} catch (UnsupportedEncodingException e) {
-			throw new IllegalStateException(e);
+			// throw new IllegalStateException(e);  // Cannot use for Java 1.4.2
+			throw new IllegalStateException(e.getMessage());
 		}
 
 		// Spec: length check
@@ -155,8 +156,10 @@ public class MqttTopic {
 		// - Topic Names and Topic Filters are UTF-8 encoded strings, they MUST
 		// NOT encode to more than 65535 bytes
 		if (topicLen < MIN_TOPIC_LEN || topicLen > MAX_TOPIC_LEN) {
-			throw new IllegalArgumentException(String.format("Invalid topic length, should be in range[%d, %d]!",
-					new Object[] { new Integer(MIN_TOPIC_LEN), new Integer(MAX_TOPIC_LEN) }));
+			//throw new IllegalArgumentException(String.format("Invalid topic length, should be in range[%d, %d]!", // Cannot use for Java 1.4.2
+			//		new Object[] { new Integer(MIN_TOPIC_LEN), new Integer(MAX_TOPIC_LEN) }));
+			String errorMessage = "Invalid topic length, should be in range["+ MIN_TOPIC_LEN + ", " + MAX_TOPIC_LEN + "]!";
+			throw new IllegalArgumentException(errorMessage);
 		}
 
 		// *******************************************************************************
@@ -177,7 +180,8 @@ public class MqttTopic {
 			// - The multi-level wildcard must be the last character used within
 			// the topic tree
 			if (Strings.countMatches(topicString, MULTI_LEVEL_WILDCARD) > 1
-					|| (topicString.contains(MULTI_LEVEL_WILDCARD) && !topicString
+					//|| (topicString.contains(MULTI_LEVEL_WILDCARD) && !topicString// Cannot use for Java 1.4.2
+					|| ((topicString.indexOf(MULTI_LEVEL_WILDCARD) != -1) && !topicString
 							.endsWith(MULTI_LEVEL_WILDCARD_PATTERN))) {
 				throw new IllegalArgumentException(
 						"Invalid usage of multi-level wildcard in topic string: "
@@ -219,9 +223,11 @@ public class MqttTopic {
             if (chars[i] == singleLevelWildcardChar) {
                 // prev and next can be only '/' or none
                 if (prev != topicLevelSeparatorChar && prev != NUL || next != topicLevelSeparatorChar && next != NUL) {
-                    throw new IllegalArgumentException(String.format(
-                            "Invalid usage of single-level wildcard in topic string '%s'!",
-                            new Object[] { topicString }));
+//                    throw new IllegalArgumentException(String.format( // Cannot use for Java 1.4.2
+//                            "Invalid usage of single-level wildcard in topic string '%s'!",
+//                            new Object[] { topicString }));
+                    String errorMessage = "Invalid usage of single-level wildcard in topic string '" + topicString + "'!";
+                    throw new IllegalArgumentException(errorMessage);
                 }
             }
         }
