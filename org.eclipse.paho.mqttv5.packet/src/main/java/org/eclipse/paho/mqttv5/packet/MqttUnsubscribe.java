1b5b799d51c6d1ee5ed117e090e41c3571c973bb
James Sutton <james.sutton@uk.ibm.com>
2016-11-25 09:44:12 +0000

    Adding MqttUnsubscribe & MqttUnsubscribeTest
    Signed-off-by: James Sutton <james.sutton@uk.ibm.com>

---
+++ org.eclipse.paho.mqttv5.packet/src/main/java/org/eclipse/paho/mqttv5/packet/MqttUnsubscribe.java
@@ -0,0 +1,160 @@
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution. 
+ *
+ * The Eclipse Public License is available at 
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ * 	  Dave Locke   - Original MQTTv3 implementation
+ *    James Sutton - Initial MQTTv5 implementation
+ */
+package org.eclipse.paho.mqttv5.packet;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.eclipse.paho.mqttv5.util.MqttException;
+
+public class MqttUnsubscribe extends MqttWireMessage{
+	
+	// Identifier / Value Identifiers
+	private static final byte REASON_STRING_IDENTIFIER = 0x1F;
+	
+	// Fields
+	private String[] topics;
+	private String reasonString;
+	
+	public  MqttUnsubscribe(byte info, byte[] data) throws IOException, MqttException {
+		super(MqttWireMessage.MESSAGE_TYPE_UNSUBSCRIBE);
+		ByteArrayInputStream bais = new ByteArrayInputStream(data);
+		CountingInputStream counter = new CountingInputStream(bais);
+		DataInputStream inputStream = new DataInputStream(counter);
+		msgId = inputStream.readUnsignedShort();
+		
+		parseIdentifierValueFields(inputStream);
+		
+		ArrayList<String> topicList = new ArrayList<>();
+		// Whilst we are reading data
+		while(counter.getCounter() < data.length){
+			topicList.add( decodeUTF8(inputStream));
+		}
+		topics = topicList.toArray(new String[topicList.size()]);
+		
+	
+		
+		inputStream.close();
+	}
+	
+	public MqttUnsubscribe(String[] topics){
+		super(MqttWireMessage.MESSAGE_TYPE_UNSUBSCRIBE);
+		this.topics = topics;
+	}
+	
+	@Override
+	protected byte[] getVariableHeader() throws MqttException {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			DataOutputStream outputStream = new DataOutputStream(baos);
+
+			// Encode the Message ID
+			outputStream.writeShort(msgId);
+
+
+			// Write Identifier / Value Fields
+			byte[] identifierValueFieldsByteArray = getIdentifierValueFields();
+			outputStream.write(encodeVariableByteInteger(identifierValueFieldsByteArray.length));
+			outputStream.write(identifierValueFieldsByteArray);
+			outputStream.flush();
+			return baos.toByteArray();
+		} catch (IOException ioe) {
+			throw new MqttException(ioe);
+		}
+	}
+	
+	@Override
+	public byte[] getPayload() throws MqttException {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			DataOutputStream outputStream = new DataOutputStream(baos);
+			
+			for(String topic : topics){
+				encodeUTF8(outputStream, topic);
+			}
+			
+			outputStream.flush();
+			return baos.toByteArray();
+		} catch (IOException ioe){
+			throw new MqttException(ioe);
+		}
+	}
+	
+	private byte[] getIdentifierValueFields() throws MqttException {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			DataOutputStream outputStream = new DataOutputStream(baos);
+
+			// If Present, encode the Reason String (3.10.2.2)
+			if (reasonString != null) {
+				outputStream.write(REASON_STRING_IDENTIFIER);
+				encodeUTF8(outputStream, reasonString);
+			}
+
+			outputStream.flush();
+			return baos.toByteArray();
+		} catch (IOException ioe) {
+			throw new MqttException(ioe);
+		}
+	}
+	
+	private void parseIdentifierValueFields(DataInputStream dis) throws IOException, MqttException {
+		// First get the length of the IV fields
+		int length = readVariableByteInteger(dis).getValue();
+		if (length > 0) {
+			byte[] identifierValueByteArray = new byte[length];
+			dis.read(identifierValueByteArray, 0, length);
+			ByteArrayInputStream bais = new ByteArrayInputStream(identifierValueByteArray);
+			DataInputStream inputStream = new DataInputStream(bais);
+			while (inputStream.available() > 0) {
+				// Get the first Byte
+				byte identifier = inputStream.readByte();
+				if (identifier == REASON_STRING_IDENTIFIER) {
+					reasonString = decodeUTF8(inputStream);
+				} else {
+					// Unidentified Identifier
+					throw new MqttException(MqttException.REASON_CODE_INVALID_IDENTIFIER);
+				}
+			}
+		}
+	}
+	
+	@Override
+	protected byte getMessageInfo() {
+		return (byte)( 2 | (this.duplicate ? 8 : 0));
+	}
+
+	public String[] getTopics() {
+		return topics;
+	}
+
+	public void setTopics(String[] topics) {
+		this.topics = topics;
+	}
+
+	public String getReasonString() {
+		return reasonString;
+	}
+
+	public void setReasonString(String reasonString) {
+		this.reasonString = reasonString;
+	}
+}
