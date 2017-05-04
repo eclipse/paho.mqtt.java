3ddb38b9e9b98d50992f2e522020cc212a426c84
prmathur <prmathur@microsoft.com>
2017-05-04 00:33:40 -0700

    Change Sec-WebSocket-Protocol to mqtt (#366)
    As per Mqtt Spec
    http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/csprd02/mqtt-v3.1.1-csprd02.html#_Toc385349863
    The client MUST include mqtt in the list of WebSocket Sub Protocols it
    offers [MQTT-6.0.0.3].
    
    Signed-off-by: prmathur-microsoft <prmathur@microsoft.com>

--- org.eclipse.paho.client.mqttv3/src/main/java/org/eclipse/paho/client/mqttv3/internal/websocket/WebSocketHandshake.java
+++ org.eclipse.paho.client.mqttv3/src/main/java/org/eclipse/paho/client/mqttv3/internal/websocket/WebSocketHandshake.java
@@ -105,7 +105,7 @@ public class WebSocketHandshake {
 			pw.print("Upgrade: websocket" + LINE_SEPARATOR);
 			pw.print("Connection: Upgrade" + LINE_SEPARATOR);
 			pw.print("Sec-WebSocket-Key: " + key + LINE_SEPARATOR);
-			pw.print("Sec-WebSocket-Protocol: mqttv3.1" + LINE_SEPARATOR);
+			pw.print("Sec-WebSocket-Protocol: mqtt" + LINE_SEPARATOR);
 			pw.print("Sec-WebSocket-Version: 13" + LINE_SEPARATOR);
 
 			String userInfo = srvUri.getUserInfo();
