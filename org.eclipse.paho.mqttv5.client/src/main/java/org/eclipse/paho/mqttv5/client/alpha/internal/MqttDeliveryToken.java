a09f928ab5e83b8cf6eab71fceb06db53e804439
Tim Ward <timothyjward@apache.org>
2017-10-23 17:37:39 +0200

    Update to the latest OSGi snapshot changes to ensure efficient use of threads
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- org.eclipse.paho.mqttv5.client/src/main/java/org/eclipse/paho/mqttv5/client/alpha/internal/MqttDeliveryToken.java
+++ org.eclipse.paho.mqttv5.client/src/main/java/org/eclipse/paho/mqttv5/client/alpha/internal/MqttDeliveryToken.java
@@ -1,21 +1,19 @@
 package org.eclipse.paho.mqttv5.client.alpha.internal;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ScheduledExecutorService;
-
 import org.eclipse.paho.mqttv5.client.alpha.IMqttCommonClient;
 import org.eclipse.paho.mqttv5.client.alpha.IMqttDeliveryToken;
 import org.eclipse.paho.mqttv5.client.alpha.IMqttMessage;
 import org.eclipse.paho.mqttv5.client.alpha.result.IMqttDeliveryResult;
 import org.eclipse.paho.mqttv5.common.MqttException;
+import org.osgi.util.promise.PromiseExecutors;
 
 public class MqttDeliveryToken<C> extends MqttToken<IMqttDeliveryResult<C>, C> implements IMqttDeliveryToken<C> {
 
 	private final IMqttMessage message;
 
-	public MqttDeliveryToken(Executor executor, ScheduledExecutorService scheduler, IMqttCommonClient client,
+	public MqttDeliveryToken(PromiseExecutors promiseExecutors, IMqttCommonClient client,
 			C userContext, int messageId, IMqttMessage message) {
-		super(executor, scheduler, client, userContext, messageId);
+		super(promiseExecutors, client, userContext, messageId);
 		this.message = message;
 	}
 
