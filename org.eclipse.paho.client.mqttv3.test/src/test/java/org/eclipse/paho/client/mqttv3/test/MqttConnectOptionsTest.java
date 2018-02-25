5704ea7d9ec224a101797997eb17c937e67dd730
Daniel Dylag <bisker@op.pl>
2018-02-25 13:56:30 +0100

    Refactor NetworkModule creation
    Simplify the module by splitting into class hierarchy
    Support custom protocols/schemes
    
    Signed-off-by: Daniel Dylag <bisker@op.pl>

--- org.eclipse.paho.client.mqttv3.test/src/test/java/org/eclipse/paho/client/mqttv3/test/MqttConnectOptionsTest.java
+++ org.eclipse.paho.client.mqttv3.test/src/test/java/org/eclipse/paho/client/mqttv3/test/MqttConnectOptionsTest.java
@@ -1,58 +1,49 @@
 package org.eclipse.paho.client.mqttv3.test;
 
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import static org.junit.Assert.*;
+import org.eclipse.paho.client.mqttv3.network.NetworkModuleFactory;
 import org.junit.Test;
 
 public class MqttConnectOptionsTest {
 
-	@Test
-	public void testValidateURI() {
-		try {
-			MqttConnectOptions.validateURI("");
-			fail("Must fail: Empty URI");
-		} catch (IllegalArgumentException e) {
-			assertEquals("Unknown scheme \"null\" of URI \"\"", e.getMessage());
-		}
-		
-		try {
-			MqttConnectOptions.validateURI("udp://localhost:1883");
-			fail("Must fail: Unknow scheme");
-		} catch (IllegalArgumentException e) {
-			assertEquals("Unknown scheme \"udp\" of URI \"udp://localhost:1883\"", e.getMessage());
-		}
-		
-		MqttConnectOptions.validateURI("tcp://localhost:1883");
-		
-		try {
-			MqttConnectOptions.validateURI("tcp://localhost:1883/mqtt");
-			fail("Must fail: URI path must be empty");
-		} catch (IllegalArgumentException e) {
-			assertEquals("URI path must be empty \"tcp://localhost:1883/mqtt\"", e.getMessage());
-		}
-		
-		MqttConnectOptions.validateURI("tcp://localhost");
-		
-		try {
-			MqttConnectOptions.validateURI("localhost:1883");
-			fail("Must fail: Unknow scheme");
-		} catch (IllegalArgumentException e) {
-			assertEquals("Unknown scheme \"localhost\" of URI \"localhost:1883\"", e.getMessage());
-		}
-		
-		try {
-			MqttConnectOptions.validateURI("localhost");
-			fail("Must fail: URI path must be empty");
-		} catch (IllegalArgumentException e) {
-			assertEquals("URI path must be empty \"localhost\"", e.getMessage());
-		}
-		
-		try {
-			MqttConnectOptions.validateURI("tcp://");
-			fail("Must fail: Can't parse string to URI");
-		} catch (IllegalArgumentException e) {
-			assertEquals("Can't parse string to URI \"tcp://\"", e.getMessage());
-		}
+	private NetworkModuleFactory networkModuleFactory = NetworkModuleFactory.getInstance();
+
+    @Test
+    public void shouldPassValidationForTcpWithLocalhostAndPort() {
+        networkModuleFactory.validateURI("tcp://localhost:1883");
+    }
+
+    @Test
+    public void shouldPassValidationForTcpWithHostnameAndPort() {
+        networkModuleFactory.validateURI("tcp://google.com:1883");
+    }
+    @Test
+    public void shouldPassValidationForTcpWithLocalhostWithoutPort() {
+        networkModuleFactory.validateURI("tcp://localhost");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+	public void shouldFailValidationForEmptyURI() {
+        networkModuleFactory.validateURI("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailValidationForUnknownScheme() {
+        networkModuleFactory.validateURI("udp://localhost:1883");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailValidationForTcpSchemeWithPath() {
+        networkModuleFactory.validateURI("tcp://localhost:1883/mqtt");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailValidationForUriWithoutScheme() {
+        networkModuleFactory.validateURI("localhost:1883");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailValidationForSchemeWithoutAddress() {
+        networkModuleFactory.validateURI("tcp://");
 	}
 
 }
