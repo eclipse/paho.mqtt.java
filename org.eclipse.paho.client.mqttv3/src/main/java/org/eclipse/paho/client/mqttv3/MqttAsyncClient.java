de3593eca51dd6c8cbf9364d22619fc0a1547005
Inderjeet Singh <inder@peel.com>
2018-04-12 14:03:34 -0700

    Ensuring checkPing carries the callback passed by the client

--- org.eclipse.paho.client.mqttv3/src/main/java/org/eclipse/paho/client/mqttv3/MqttAsyncClient.java
+++ org.eclipse.paho.client.mqttv3/src/main/java/org/eclipse/paho/client/mqttv3/MqttAsyncClient.java
@@ -1006,7 +1006,7 @@ public class MqttAsyncClient implements IMqttAsyncClient {
 		// @TRACE 117=>
 		log.fine(CLASS_NAME, methodName, "117");
 
-		token = comms.checkForActivity();
+		token = comms.checkForActivity(callback);
 		// @TRACE 118=<
 		log.fine(CLASS_NAME, methodName, "118");
 
