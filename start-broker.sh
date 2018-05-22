10f1924d20fb0e556348c6038cb1df7605bc909b
James Sutton <james.sutton@uk.ibm.com>
2018-05-22 13:52:15 +0100

    Changing the max packet size for test broker v5 tests
    Signed-off-by: James Sutton <james.sutton@uk.ibm.com>

--- start-broker.sh
+++ start-broker.sh
@@ -34,8 +34,9 @@ else
 	echo "Installing and starting Python Interop Broker."
 	sudo service mosquitto stop
 	git clone https://github.com/eclipse/paho.mqtt.testing.git
+	cp java_client_testing.conf paho.mqtt.testing/interoperability/.
 	cd paho.mqtt.testing/interoperability
-	python3 startbroker.py -c client_testing.conf &
+	python3 startbroker.py -c java_client_testing.conf &
 fi
 
 
