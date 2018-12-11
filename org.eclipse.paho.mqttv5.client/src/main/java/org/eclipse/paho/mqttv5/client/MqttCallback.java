4b082722903af1b3bb49c5e2a8585af6eac6dcac
Ian Craggs <icraggs@uk.ibm.com>
2018-12-11 23:34:36 +0000

    Fix some copyright headers

--- org.eclipse.paho.mqttv5.client/src/main/java/org/eclipse/paho/mqttv5/client/MqttCallback.java
+++ org.eclipse.paho.mqttv5.client/src/main/java/org/eclipse/paho/mqttv5/client/MqttCallback.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2014 IBM Corp.
+ * Copyright (c) 2009, 2018 IBM Corp.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,6 +12,7 @@
  *
  * Contributors:
  *    Dave Locke - initial API and implementation and/or initial documentation
+ *    James Sutton - MQTT V5 implementation 
  */
 package org.eclipse.paho.mqttv5.client;
 
