222c652552c31ad8cf2cd83e11375a838a842bb8
Ian Craggs <icraggs@uk.ibm.com>
2019-02-08 17:56:05 +0000

    Add an MQTT V3 test

--- org.eclipse.paho.client.mqttv3.test/src/test/java/org/eclipse/paho/client/mqttv3/test/SendReceiveAsyncTest.java
+++ org.eclipse.paho.client.mqttv3.test/src/test/java/org/eclipse/paho/client/mqttv3/test/SendReceiveAsyncTest.java
@@ -22,8 +22,10 @@ import java.util.logging.Logger;
 import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.test.client.MqttClientFactoryPaho;
 import org.eclipse.paho.client.mqttv3.test.logging.LoggingUtilities;
 import org.eclipse.paho.client.mqttv3.test.properties.TestProperties;
@@ -732,7 +734,50 @@ public class SendReceiveAsyncTest {
         log.log(Level.SEVERE, "caught exception:", exception);
       }
     }
-
     log.exiting(className, methodName);
-  }
+   }
+    
+    @Test
+	public void testPublishManyQoS0Messages() throws Exception {
+		String methodName = Utility.getMethodName();
+		LoggingUtilities.banner(log, cclass, methodName);
+		String clientId = methodName;
+		IMqttAsyncClient asyncClient = new MqttAsyncClient(serverURI.toString(), clientId);
+
+		// Connect to the server
+		log.info("Connecting: [serverURI: " + serverURI + ", ClientId: " + clientId + "]");
+		IMqttToken connectToken = asyncClient.connect();
+		connectToken.waitForCompletion(5000);
+		String clientId2 = asyncClient.getClientId();
+		log.info("Client ID = " + clientId2);
+		boolean isConnected = asyncClient.isConnected();
+		log.info("isConnected: " + isConnected);
+		
+		MqttMessage testMessage = new MqttMessage("Test Payload".getBytes());
+		testMessage.setQos(0);
+		testMessage.setRetained(false);
+		long lStartTime = System.nanoTime();
+		int no_of_messages = 70000; 
+		for(int i = 0; i < no_of_messages; i++) {
+			IMqttDeliveryToken deliveryToken = asyncClient.publish(topicPrefix + methodName, testMessage);
+			try
+			{
+				deliveryToken.waitForCompletion(5000);
+			} catch (Exception e) {
+				System.out.println("wait failed "+i);
+			}
+		}
+
+        long lEndTime = System.nanoTime();
+        long output = lEndTime - lStartTime; 		//time elapsed
+        log.info("Sending "+no_of_messages+" of messages  took : " + output / 1000000 + " milliseconds.");
+
+		log.info("Disconnecting...");
+		IMqttToken disconnectToken = asyncClient.disconnect();
+		disconnectToken.waitForCompletion(5000);
+		Assert.assertFalse(asyncClient.isConnected());
+		asyncClient.close();
+
+	}
+
 }
