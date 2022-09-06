a87634079518e57de56ee7e2932101944f547976
yslai <yslai@neldtv.org>
2022-09-06 10:18:01 +0800

    add EPL2.0
    Signed-off-by: lys1737 <296343318@qq.com>

--- org.eclipse.paho.client.mqttv3/src/main/java/org/eclipse/paho/client/mqttv3/PasswdHandler.java
+++ org.eclipse.paho.client.mqttv3/src/main/java/org/eclipse/paho/client/mqttv3/PasswdHandler.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    https://www.eclipse.org/legal/epl-2.0
+ * and the Eclipse Distribution License is available at
+ *   https://www.eclipse.org/org/documents/edl-v10.php
+ *
+ */
 package org.eclipse.paho.client.mqttv3;
 
 
